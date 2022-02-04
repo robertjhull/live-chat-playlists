327f4192ba074a055b4236bd4afa164eff125add
Robert Hull <robertjhull@protonmail.com>
2022-02-03 18:23:13 -0800

    Implement playlist manager framework

---
+++ LiveChatPlaylists/Playlist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveChatPlaylists
+{
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public List<string> Keywords { get; set; }
+        private List<string> _videoIDs { get; set; }
+        public Playlist(string name, List<string> keywords)
+        {
+            Name = name;
+            Keywords = keywords;
+        }
+        public void AddVideosToPlaylist(List<string> videoIDs)
+        {
+            _videoIDs.Concat(videoIDs);
+        }
+
+        public void ResetVideosInPlaylist(List<string> videoIDs)
+        {
+            _videoIDs = videoIDs;
+        }
+    }
+}
